314cd2f50a4c2275514b1263a4b6b2057a599bb6
Rémi Van Keisbelck <remi@rvkb.com>
2022-12-04 00:45:46 +0100

    move most of the meat out of sandbox

--- monaco/src/index.ts
+++ monaco/src/index.ts
@@ -2,6 +2,14 @@ import * as vscode from 'vscode';
 import { CompletionItemKind} from 'vscode-languageserver-protocol';
 import {DieselParserFacade} from "@diesel-parser/ts-facade";
 import { TextDocument } from 'vscode-languageserver-textdocument';
+import {Diagnostic, DiagnosticSeverity} from 'vscode-languageserver-protocol';
+import {createConverter as createProtocolConverter} from 'vscode-languageclient/lib/common/protocolConverter.js';
+import {StandaloneServices} from 'vscode/services';
+import getMessageServiceOverride from 'vscode/service-override/messages';
+
+StandaloneServices.initialize({
+    ...getMessageServiceOverride(document.body)
+});
 
 function createDocument(modelUri: string,vscodeDocument: vscode.TextDocument) {
     return TextDocument.create(modelUri, vscodeDocument.languageId, vscodeDocument.version, vscodeDocument.getText());
@@ -67,4 +75,81 @@ export function registerSemanticHighlight(
             tokenModifiers: []
         }
     );
+}
+
+const pendingValidationRequests = new Map<string, number>();
+
+export function validate(modelUri: string, monacoUri: vscode.Uri, vscodeDocument: vscode.TextDocument, parser: DieselParserFacade): void {
+    const document = createDocument(modelUri, vscodeDocument);
+    cleanPendingValidation(document);
+    pendingValidationRequests.set(document.uri, window.setTimeout(() => {
+        pendingValidationRequests.delete(document.uri);
+        doValidate(monacoUri, parser, document);
+    }));
+}
+
+function cleanPendingValidation(document: TextDocument): void {
+    const request = pendingValidationRequests.get(document.uri);
+    if (request !== undefined) {
+        window.clearTimeout(request);
+        pendingValidationRequests.delete(document.uri);
+    }
+}
+
+const SEV_MAP: { [key: string]: DiagnosticSeverity } = {
+    "info": DiagnosticSeverity.Information,
+    "warning": DiagnosticSeverity.Warning,
+    "error": DiagnosticSeverity.Error
+};
+
+const protocolConverter = createProtocolConverter(undefined, true, true);
+const diagnosticCollection = vscode.languages.createDiagnosticCollection('diesel');
+
+function doValidate(monacoUri: vscode.Uri, parser: DieselParserFacade, document: TextDocument): void {
+
+    const text = document.getText();
+    if (text.length === 0) {
+        cleanDiagnostics();
+        return;
+    }
+
+    const parseResult = parser.parse({text});
+    if (!parseResult.success) {
+        console.error("unable to parse", parseResult.error);
+        return;
+    }
+
+    const diagnostics: Diagnostic[] = [];
+    if (parseResult.success) {
+        // it's ok, look for markers
+        parseResult.markers.forEach(marker => {
+            diagnostics.push({
+                message: marker.getMessage("en"), // TODO
+                range: {
+                    start: document.positionAt(marker.offset),
+                    end: document.positionAt(marker.offset + marker.length)
+                },
+                severity: SEV_MAP[marker.severity] ?? DiagnosticSeverity.Error
+            });
+        });
+    } else {
+        // parsing error
+        // TODO better logging
+        diagnostics.push({
+            message: parseResult.error ?? "Unhandled parsing error :/",
+            range: {
+                start: document.positionAt(0),
+                end: document.positionAt(text.length)
+            },
+            severity: DiagnosticSeverity.Error
+        });
+    }
+
+    protocolConverter.asDiagnostics(diagnostics).then(ds => {
+        diagnosticCollection.set(monacoUri, ds);
+    });
+}
+
+function cleanDiagnostics(): void {
+    diagnosticCollection.clear();
 }
--- sandbox/src/client.ts
+++ sandbox/src/client.ts
@@ -1,8 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) 2018-2022 TypeFox GmbH (http://www.typefox.io). All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import 'monaco-editor/esm/vs/editor/editor.all.js';
 
 // support all editor features
@@ -21,25 +16,13 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
 import * as vscode from 'vscode';
 
 import {buildWorkerDefinition} from 'monaco-editor-workers';
-
-import {createConverter as createProtocolConverter} from 'vscode-languageclient/lib/common/protocolConverter.js';
-import {StandaloneServices} from 'vscode/services';
-import getMessageServiceOverride from 'vscode/service-override/messages';
-import {TextDocument} from 'vscode-languageserver-textdocument';
-import {Diagnostic, DiagnosticSeverity} from 'vscode-languageserver-protocol';
 import {DieselParserFacade} from '@diesel-parser/ts-facade';
 
 // @ts-ignore
 import {DieselSamples} from '@diesel-parser/samples';
-import {registerCompletion, registerSemanticHighlight} from "@diesel-parser/monaco";
-
-StandaloneServices.initialize({
-    ...getMessageServiceOverride(document.body)
-});
+import {registerCompletion, registerSemanticHighlight, validate} from "@diesel-parser/monaco";
 
 buildWorkerDefinition('dist', new URL('', window.location.href).href, false);
-// const codeConverter = createCodeConverter();
-const protocolConverter = createProtocolConverter(undefined, true, true);
 
 const LANGUAGE_ID = 'bmd';
 const MODEL_URI = 'inmemory://model.bmd';
@@ -85,96 +68,13 @@ function getTokenType(styleName: string): string | undefined {
 
 const tokenTypes = ['number', 'string', 'keyword', 'property'];
 
-function createDocument(vscodeDocument: vscode.TextDocument) {
-    return TextDocument.create(MODEL_URI, vscodeDocument.languageId, vscodeDocument.version, vscodeDocument.getText());
-}
-
 // @ts-ignore
 const dieselParser: DieselParserFacade = DieselSamples.createBmdParser();
 
 registerCompletion(MODEL_URI, LANGUAGE_ID, () => dieselParser);
 registerSemanticHighlight(LANGUAGE_ID, () => dieselParser, tokenTypes, getTokenType);
 
-const pendingValidationRequests = new Map<string, number>();
-
 model.onDidChangeContent((_event) => {
-    validate();
+    validate(MODEL_URI, MONACO_URI, vscodeDocument, dieselParser);
 });
-validate();
-// debugger;
-// dieselMonaco.validate(vscodeDocument);
-
-
-
-function validate(): void {
-    const document = createDocument(vscodeDocument);
-    cleanPendingValidation(document);
-    pendingValidationRequests.set(document.uri, window.setTimeout(() => {
-        pendingValidationRequests.delete(document.uri);
-        doValidate(document);
-    }));
-}
-
-function cleanPendingValidation(document: TextDocument): void {
-    const request = pendingValidationRequests.get(document.uri);
-    if (request !== undefined) {
-        window.clearTimeout(request);
-        pendingValidationRequests.delete(document.uri);
-    }
-}
-
-const SEV_MAP: { [key: string]: DiagnosticSeverity } = {
-	"info": DiagnosticSeverity.Information,
-	"warning": DiagnosticSeverity.Warning,
-	"error": DiagnosticSeverity.Error
-};
-
-const diagnosticCollection = vscode.languages.createDiagnosticCollection('diesel');
-function doValidate(document: TextDocument): void {
-
-    const text = document.getText();
-    if (text.length === 0) {
-        cleanDiagnostics();
-        return;
-    }
-
-    const parseResult = dieselParser.parse({text});
-    if (!parseResult.success) {
-        console.error("unable to parse", parseResult.error);
-        return;
-    }
-
-    const diagnostics: Diagnostic[] = [];
-	if (parseResult.success) {
-		// it's ok, look for markers
-		parseResult.markers.forEach(marker => {
-			diagnostics.push({
-                message: marker.getMessage("en"), // TODO
-                range: {
-                    start: document.positionAt(marker.offset),
-                    end: document.positionAt(marker.offset + marker.length)
-                },
-                severity: SEV_MAP[marker.severity] ?? DiagnosticSeverity.Error
-            });
-		});
-	} else {
-		// parsing error
-		// TODO better logging
-		diagnostics.push({
-            message: parseResult.error ?? "Unhandled parsing error :/",
-            range: {
-                start: document.positionAt(0),
-                end: document.positionAt(text.length)
-            },
-            severity: DiagnosticSeverity.Error
-        });
-	}
-
-   protocolConverter.asDiagnostics(diagnostics).then(ds => {
-        diagnosticCollection.set(MONACO_URI, ds);
-    });
-}
-
-function cleanDiagnostics(): void {
-    diagnosticCollection.clear();
-}
+validate(MODEL_URI, MONACO_URI, vscodeDocument, dieselParser);
